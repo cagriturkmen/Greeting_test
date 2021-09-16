0232e9f4d97869fd09f6e7478ceffcd9486bb669
cagriturkmen <c.turkmen2@hotmail.com>
2021-09-16 20:49:25 +0300

    first commit

---
+++ src/com/GreetingTest/Greeting.java
@@ -0,0 +1,12 @@
+package com.GreetingTest;
+
+public class Greeting {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Yeni komutlar
+		// aasda
+		// Asa
+	}
+	
+}
